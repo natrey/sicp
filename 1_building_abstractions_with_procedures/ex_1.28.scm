6f2fc1272e1e32ba909d113a65c9bff9322ad0bc
n.andreychenko <natandreychenko@gmail.com>
2021-04-28 14:00:25 +0300

    ADD ex_1.28.scm

---
+++ 1_building_abstractions_with_procedures/ex_1.28.scm
@@ -0,0 +1,70 @@
+#|
+    One variant of the Fermat test that cannot be fooled is called the Miller-Rabin test (Miller 1976; Rabin 1980).
+
+    This starts from an alternate form of Fermat's Little Theorem, 
+    which states that if `n` is a prime number and `a` is any positive integer less than `n`, 
+    then `a` raised to the (n - 1)st power is congruent to 1 modulo `n`. 
+    To test the primality of a number `n` by the Miller-Rabin test, 
+    we pick a random number a<n and raise `a` to the (n - 1)st power modulo `n` using the expmod procedure.
+
+    However, whenever we perform the squaring step in expmod, 
+    we check to see if we have discovered a "nontrivial square root of 1 modulo n," 
+    that is, a number not equal to 1 or n - 1 whose square is equal to 1 modulo n. 
+    It is possible to prove that if such a nontrivial square root of 1 exists, 
+    then `n` is not prime. It is also possible to prove that if `n` is an odd number that is not prime, 
+    then, for at least half the numbers a<n, 
+    computing a^(n-1) in this way will reveal a nontrivial square root of 1 modulo n. 
+    (This is why the Miller-Rabin test cannot be fooled.) 
+    
+    Modify the expmod procedure to signal if it discovers a nontrivial square root of 1, 
+    and use this to implement the Miller-Rabin test with a procedure analogous to fermat-test. 
+    Check your procedure by testing various known primes and non-primes. 
+    Hint: One convenient way to make expmod signal is to have it return 0.
+ |#
+
+(define (square a) 
+    (* a a))
+
+(define (nontrivial-square-root-check a n)
+    (cond ((= a 1) true)
+          ((= a (- n 1)) true)
+          (else (not (= (remainder (square a)
+                        n)
+                   1)))))
+
+(define (square-check a n)
+    (if (nontrivial-square-root-check a n) 
+      (* a a) 
+      0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+            (remainder (square-check (expmod base (/ exp 2) m) m)
+                       m))
+        (else
+            (remainder (* base (expmod base (- exp 1) m))
+                       m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((miller-rabin-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 36 100) ;#f
+(fast-prime? 1999 1000) ;#t
+(fast-prime? 10037 10000) ;#t
+(fast-prime? 19999 1000) ;#f
+
+;Carmichael numbers
+(fast-prime? 561 1000)  ;#f
+(fast-prime? 1105 10000) ;#f
+(fast-prime? 1729 10000) ;#f
+(fast-prime? 2465 10000) ;#f
+(fast-prime? 2821 10000) ;#f
+(fast-prime? 6601 10000) ;#f
