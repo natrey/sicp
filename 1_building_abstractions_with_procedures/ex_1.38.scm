c4434b4ea67516fc65af7f3f09b6e34ef67b63b9
n.andreychenko <natandreychenko@gmail.com>
2021-06-07 17:29:57 +0300

    ADD ex_1.38.scm

---
+++ 1_building_abstractions_with_procedures/ex_1.38.scm
@@ -0,0 +1,31 @@
+#|
+  In 1737, the Swiss mathematician Leonhard Euler published a memoir `De Fractionibus Continuis`, 
+  which included a continued fraction expansion for `e - 2`, where `e` is the base of the natural logarithms. 
+  In this fraction, the Ni are all 1, and the Di are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, .... 
+  Write a program that uses your `cont-frac` procedure from exercise 1.37 to approximate `e`, based on Euler's expansion.
+ |#
+(define (euler-n x)
+        (cond ((= x 0) 1)
+              ((= (remainder (- x 1) 3) 
+                  0) (* 2 
+                        (+ 1 
+                           (/ (- x 1) 
+                              3))))
+              (else 1)))
+
+(euler-n 10) ;8
+(euler-n 7)  ;6
+(euler-n 2)  ;1
+
+(define (cont-frac-iter n d k)
+    (define (iter i result)
+        (if (= i k)
+            result
+            (iter (+ i 1) (/ (n i)
+                             (+ (d i)
+                                result)))))
+    (iter 0 0))
+
+(cont-frac-iter (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           11)
